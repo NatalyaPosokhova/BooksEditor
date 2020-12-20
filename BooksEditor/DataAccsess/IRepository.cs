ce34a83dd1d92ee54419ad131dff76e57cb2e734
Natalya <Natalya.Posokhova@gmail.com>
2020-12-21 00:57:51 +0500

    add interface with the main func of communication with db.

---
+++ BooksEditor/DataAccsess/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using BooksEditor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksEditor.DataAccsess
+{
+    public interface IRepository
+    {
+        public IEnumerable<Book> GetAll();
+        public void Add(Book book);
+        public Book Find(int id);
+        public void Update(Book book);
+        public void Remove(int id);
+    }
+}

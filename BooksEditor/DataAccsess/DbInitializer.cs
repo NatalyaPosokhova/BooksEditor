479088ab1275e5cd0e99421fc98ed8e021e5721b
Natalya <Natalya.Posokhova@gmail.com>
2020-12-21 00:09:06 +0500

    add initializer to fill table if database is empty

---
+++ BooksEditor/DataAccsess/DbInitializer.cs
@@ -0,0 +1,45 @@
+﻿using BooksEditor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksEditor.DataAccsess
+{
+    public class DbInitializer
+    {
+        public static void Initialize(BooksEditorContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Books.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var books = new Book[]
+            {
+            new Book{ Title = "The Adventures of Tom Sawyer", PagesNumber = 1000, Publisher = "HarperCollins Publishers", ReleaseYear = 1876},
+            new Book{ Title = "Winnie the Pooh", PagesNumber = 300, Publisher = "Random House Inc.", ReleaseYear = 1926}
+            };
+            foreach (Book book in books)
+            {
+                context.Books.Add(book);
+            }
+            context.SaveChanges();
+
+            var authors = new Author[]
+            {
+            new Author { FirstName = "Mark", LastName = "Twain" },
+            new Author { FirstName = "Alan", LastName = "Miln" }
+            };
+            foreach (Author author in authors)
+            {
+                context.Authors.Add(author);
+            }
+            context.SaveChanges();
+
+        }
+    }
+}

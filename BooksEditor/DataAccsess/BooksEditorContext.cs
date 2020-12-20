06e226301a245131323f26954235b00930e64976
Natalya <Natalya.Posokhova@gmail.com>
2020-12-20 22:22:21 +0500

    create context

---
+++ BooksEditor/DataAccsess/BooksEditorContext.cs
@@ -0,0 +1,23 @@
+﻿using BooksEditor.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksEditor.DataAccsess
+{
+    public class BooksEditorContext : DbContext
+    {
+        public BooksEditorContext(DbContextOptions<BooksEditorContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+    }
+}

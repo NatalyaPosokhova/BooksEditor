154c2697333066bbad3e7059f3cb9ce59b1b2a67
Natalya <Natalya.Posokhova@gmail.com>
2020-12-21 17:10:39 +0500

    realize add book data to db method

---
+++ BooksEditor/DataAccsess/BooksEditorRepository.cs
@@ -0,0 +1,42 @@
+﻿using BooksEditor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksEditor.DataAccsess
+{
+    public class BooksEditorRepository : IRepository
+    {
+        private BooksEditorContext _context { get; set; }
+        public BooksEditorRepository(BooksEditorContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBookData(Book book)
+        {
+            _context.Add(book);
+        }
+
+        public Book Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
